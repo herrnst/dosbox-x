2d45b62a74b31db4bdb967c6189d66cfcdd9dc0a
Daniel Scheller <d.scheller@gmx.net>
2025-08-14 23:44:37 +0200

    Revert all CMOS changes back to (and including) 042d05f
    This does a complete revert including all related future commits of 042d05f
    which changes the CMOS date/time emulation to something that should more
    closely match what real MS-DOS does. Though, this causes issues with some
    software not working correctly anymore which all were bisected back to the
    mentioned commit. The main reason for the revert is
    https://github.com/joncampbell123/dosbox-x/issues/4725 (old NT-based OSes
    not being able to properly load boot configuration changes like video driver
    updates and such, making them hard to use properly, while DOSBox-X is able
    to - while using a few quirks for the installation - perfectly run Windows
    NT up to and including Windows XP in an emulated machine).
    
    Since the upstream developer doesn't seem to really care about this
    regression and having the need for an (emulated) NT machine, this revert is
    the workaround for that issue.

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -7846,7 +7846,6 @@ void IDE_Init();
 void NE2K_Init();
 void FDC_Primary_Init();
 void AUTOEXEC_Init();
-void DOS_InitClock();
 
 #if defined(WIN32)
 // NTS: I intend to add code that not only indicates High DPI awareness but also queries the monitor DPI
@@ -7940,9 +7939,6 @@ bool VM_Boot_DOSBox_Kernel() {
         if (IS_PC98_ARCH) update_pc98_function_row(1);
 
         DispatchVMEvent(VM_EVENT_DOS_INIT_KERNEL_READY); // <- kernel is ready
-
-        /* Date/time */
-        DOS_InitClock();
 
 #if !defined(OSFREE)
         /* keyboard mapping, at this point in CONFIG.SYS parsing, right? */
